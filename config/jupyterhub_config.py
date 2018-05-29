4ed1bab0d738a7eae4f5803450398cc02393c9e0
Bowen Sun <bowensun@gmail.com>
2018-05-29 23:18:36 +0100

    debug error 400 on terminals/websocket

--- config/jupyterhub_config.py
+++ config/jupyterhub_config.py
@@ -11,8 +11,10 @@ c.Authenticator.enable_auth_state = True
 c.Authenticator.admin_users = set([os.environ['ADMIN_USERS']])
 c.JupyterHub.spawner_class = 'dockerspawner.DockerSpawner'
 c.DockerSpawner.image = os.environ['DOCKER_SPAWNER_IMAGE']
+c.DockerSpawner.remove_containers = True
 c.Spawner.start_timeout = 300
 c.Spawner.env_keep = ['JPYNB_S3_ACCESS_KEY_ID', 'JPYNB_S3_SECRET_ACCESS_KEY', 'JPYNB_S3_REGION_NAME', 'JPYNB_S3_BUCKET_NAME']
+c.Spawner.args = ['--allow-root', '--debug', '--SingleUserNotebookApp.allow_origin_pat=\'*\'']
 
 from subprocess import check_call
 def docker_init(spawner):
