079f0265592f8613137c8ea2f84162b65fe7fc56
Bowen Sun <bowensun@gmail.com>
2018-05-30 01:07:53 +0100

    debug

--- config/jupyterhub_config.py
+++ config/jupyterhub_config.py
@@ -14,7 +14,7 @@ c.DockerSpawner.image = os.environ['DOCKER_SPAWNER_IMAGE']
 c.DockerSpawner.remove_containers = True
 c.Spawner.start_timeout = 300
 c.Spawner.env_keep = ['JPYNB_S3_ACCESS_KEY_ID', 'JPYNB_S3_SECRET_ACCESS_KEY', 'JPYNB_S3_REGION_NAME', 'JPYNB_S3_BUCKET_NAME']
-c.Spawner.args = ['--allow-root', '--debug']
+c.Spawner.args = ['--allow-root', '--debug', '--SingleUserNotebookApp.allow_origin=\'*\'']
 
 from subprocess import check_call
 def docker_init(spawner):
