2bf0144fc6cd4794dfd1384587fc9f4525b6c157
Bowen Sun <bowensun@gmail.com>
2018-05-31 16:29:23 +0100

    fix Fargate error

--- config/jupyterhub_config.py
+++ config/jupyterhub_config.py
@@ -1,9 +1,9 @@
 import os
-import commands
+import socket
 from oauthenticator.generic import GenericOAuthenticator
 c.JupyterHub.authenticator_class = GenericOAuthenticator
 
-c.JupyterHub.bind_url = commands.getoutput("hostname -I")
+c.JupyterHub.bind_url = 'http://' + socket.gethostbyname(socket.gethostname()) + ':8000'
 c.Authenticator.auto_login = True
 c.Authenticator.enable_auth_state = True
 c.Authenticator.admin_users = set([os.environ['ADMIN_USERS']])
