c82b893b30a7785592a319c18318b52f4c4028b1
Bowen Sun <bowensun@gmail.com>
2018-05-31 23:16:00 +0100

    fix jupyterhub-singleuser path

--- config/jupyterhub_config.py
+++ config/jupyterhub_config.py
@@ -9,7 +9,8 @@ c.Authenticator.auto_login = True
 c.Authenticator.enable_auth_state = True
 c.Authenticator.admin_users = set([os.environ['ADMIN_USERS']])
 c.JupyterHub.db_url = os.environ.get('DB_URL', 'sqlite:///jupyterhub.sqlite')
-c.Spawner.env_keep = ['JPYNB_S3_ACCESS_KEY_ID', 'JPYNB_S3_SECRET_ACCESS_KEY', 'JPYNB_S3_REGION_NAME', 'JPYNB_S3_BUCKET_NAME']
+c.Spawner.cmd = ['/opt/conda/bin/jupyterhub-singleuser']
+c.Spawner.env_keep = ['PATH', 'JPYNB_S3_ACCESS_KEY_ID', 'JPYNB_S3_SECRET_ACCESS_KEY', 'JPYNB_S3_REGION_NAME', 'JPYNB_S3_BUCKET_NAME']
 
 from subprocess import check_call
 def spawner_init(spawner):
