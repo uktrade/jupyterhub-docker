7cd08c4bb1b8697e5bc627321393b8fa8dba9f76
Bowen Sun <bowensun@gmail.com>
2018-06-01 17:55:11 +0100

    remove debug code

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
-
 set -euo pipefail
-#debug
-env | sort
 
 cat <<EOF >/etc/jupyter/ssl.key
 $SSL_KEY
@@ -11,8 +8,5 @@ EOF
 cat <<EOF > /etc/jupyter/ssl.crt
 $SSL_CERT
 EOF
-
-cat /etc/jupyter/ssl.key
-cat /etc/jupyter/ssl.crt
 
 jupyterhub --config=/etc/jupyter/jupyterhub_config.py
