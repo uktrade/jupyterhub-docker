124a0328dc3e2c84887be08995faea045112de4e
Bowen Sun <bowensun@gmail.com>
2018-06-01 16:42:50 +0100

    debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,7 @@ env | sort
 echo -e $SSL_KEY > /etc/jupyter/ssl.key
 echo -e $SSL_CERT > /etc/jupyter/ssl.crt
 
+cat /etc/jupyter/ssl.key
+cat /etc/jupyter/ssl.crt
+
 jupyterhub --config=/etc/jupyter/jupyterhub_config.py
