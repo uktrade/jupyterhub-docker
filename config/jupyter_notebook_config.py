d0bfdc3dad6f4f21882baa9106ad3bc92dd1dec5
Bowen Sun <bowensun@gmail.com>
2018-06-08 14:19:17 +0100

    cleanup

--- config/jupyter_notebook_config.py
+++ config/jupyter_notebook_config.py
@@ -2,8 +2,8 @@ import os
 from s3contents import S3ContentsManager
 c = get_config()
 
-c.NotebookApp.contents_manager_class = S3ContentsManager
 c.NotebookApp.terminals_enabled = False
+c.NotebookApp.contents_manager_class = S3ContentsManager
 c.S3ContentsManager.access_key_id = os.environ['JPYNB_S3_ACCESS_KEY_ID']
 c.S3ContentsManager.secret_access_key = os.environ['JPYNB_S3_SECRET_ACCESS_KEY']
 c.S3ContentsManager.region_name= os.environ['JPYNB_S3_REGION_NAME']
