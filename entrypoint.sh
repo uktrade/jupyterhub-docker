d05dc2497b016f255775ef9bc949e2400a6bf02f
Bowen Sun <bowensun@gmail.com>
2018-07-04 10:28:40 +0100

    update base image

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-cat <<EOF >/etc/jupyter/ssl.key
+cat <<EOF > /etc/jupyter/ssl.key
 $SSL_KEY
 EOF
 
@@ -9,4 +9,5 @@ cat <<EOF > /etc/jupyter/ssl.crt
 $SSL_CERT
 EOF
 
+docker pull $DOCKER_SPAWNER_IMAGE
 jupyterhub --config=/etc/jupyter/jupyterhub_config.py
