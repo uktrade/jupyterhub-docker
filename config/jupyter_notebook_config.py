c2512912bc1dd7436ac9d28e38352f969cf459d9
Bowen Sun <bowensun@gmail.com>
2018-08-22 16:24:48 +0100

    fix notebook config

--- config/jupyter_notebook_config.py
+++ config/jupyter_notebook_config.py
@@ -4,9 +4,9 @@ c = get_config()
 
 c.NotebookApp.terminals_enabled = False
 c.NotebookApp.contents_manager_class = S3ContentsManager
-c.S3ContentsManager.access_key_id = __JPYNB_S3_ACCESS_KEY_ID__
-c.S3ContentsManager.secret_access_key = __JPYNB_S3_SECRET_ACCESS_KEY__
-c.S3ContentsManager.region_name= __JPYNB_S3_REGION_NAME__
-c.S3ContentsManager.bucket = __JPYNB_S3_BUCKET_NAME__
+c.S3ContentsManager.access_key_id = "__JPYNB_S3_ACCESS_KEY_ID__"
+c.S3ContentsManager.secret_access_key = "__JPYNB_S3_SECRET_ACCESS_KEY__"
+c.S3ContentsManager.region_name= "__JPYNB_S3_REGION_NAME__"
+c.S3ContentsManager.bucket = "__JPYNB_S3_BUCKET_NAME__"
 c.S3ContentsManager.prefix = os.environ['JUPYTERHUB_USER']
 c.S3ContentsManager.sse = "AES256"
