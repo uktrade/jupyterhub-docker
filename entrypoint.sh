41960181cf1a9d253438584f97f43d6c5e10cdb2
Bowen Sun <bowensun@gmail.com>
2018-08-22 11:29:35 +0100

    fix sed with unescaped chars

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,10 +9,10 @@ cat <<EOF > /etc/jupyter/ssl.crt
 $SSL_CERT
 EOF
 
-sed -i "s/__JPYNB_S3_ACCESS_KEY_ID__/$JPYNB_S3_ACCESS_KEY_ID/g" /etc/jupyter/jupyter_notebook_config.py
-sed -i "s/__JPYNB_S3_SECRET_ACCESS_KEY__/$JPYNB_S3_SECRET_ACCESS_KEY/g" /etc/jupyter/jupyter_notebook_config.py
-sed -i "s/__JPYNB_S3_REGION_NAME__/$JPYNB_S3_REGION_NAME/g" /etc/jupyter/jupyter_notebook_config.py
-sed -i "s/__JPYNB_S3_BUCKET_NAME__/$JPYNB_S3_BUCKET_NAME/g" /etc/jupyter/jupyter_notebook_config.py
+sed -i -e "s%__JPYNB_S3_ACCESS_KEY_ID__%${JPYNB_S3_ACCESS_KEY_ID}%g" /etc/jupyter/jupyter_notebook_config.py
+sed -i -e "s%__JPYNB_S3_SECRET_ACCESS_KEY__%${JPYNB_S3_SECRET_ACCESS_KEY}%g" /etc/jupyter/jupyter_notebook_config.py
+sed -i -e "s%__JPYNB_S3_REGION_NAME__%${JPYNB_S3_REGION_NAME}%g" /etc/jupyter/jupyter_notebook_config.py
+sed -i -e "s%__JPYNB_S3_BUCKET_NAME__%${JPYNB_S3_BUCKET_NAME}%g" /etc/jupyter/jupyter_notebook_config.py
 
 docker pull $DOCKER_SPAWNER_IMAGE
 jupyterhub --config=/etc/jupyter/jupyterhub_config.py
