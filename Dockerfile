f2eec7383ab25de62894d5a29a78bf839b71812b
Bowen Sun <bowensun@gmail.com>
2018-08-22 15:03:06 +0100

    share config files from host

--- Dockerfile
+++ Dockerfile
@@ -5,10 +5,9 @@ ENV DOCKER_VER=18.03.1
 RUN pip install oauthenticator dockerspawner psycopg2-binary
 RUN wget -q -O - "https://download.docker.com/linux/static/stable/x86_64/docker-${DOCKER_VER}-ce.tgz" | tar -xzvf - -C /usr/bin --strip-components=1
 
-COPY config/jupyterhub_config.py /etc/jupyter/jupyterhub_config.py
-COPY config/jupyter_notebook_config.py /etc/jupyter/jupyter_notebook_config.py
-COPY config/spawner-init.sh /etc/jupyter/spawner-init.sh
-VOLUME /etc/jupyter
+COPY config/jupyterhub_config.py /usr/local/etc/jupyter/jupyterhub_config.py
+COPY config/jupyter_notebook_config.py /usr/local/etc/jupyter/jupyter_notebook_config.py
+COPY config/spawner-init.sh /usr/local/etc/jupyter/spawner-init.sh
 COPY entrypoint.sh /entrypoint.sh
 
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-cat <<EOF > /etc/jupyter/ssl.key
+cat <<EOF > /usr/local/etc/jupyter/ssl.key
 $SSL_KEY
 EOF
 
-cat <<EOF > /etc/jupyter/ssl.crt
+cat <<EOF > /usr/local/etc/jupyter/ssl.crt
 $SSL_CERT
 EOF
 
-sed -i -e "s%__JPYNB_S3_ACCESS_KEY_ID__%${JPYNB_S3_ACCESS_KEY_ID}%g" /etc/jupyter/jupyter_notebook_config.py
-sed -i -e "s%__JPYNB_S3_SECRET_ACCESS_KEY__%${JPYNB_S3_SECRET_ACCESS_KEY}%g" /etc/jupyter/jupyter_notebook_config.py
-sed -i -e "s%__JPYNB_S3_REGION_NAME__%${JPYNB_S3_REGION_NAME}%g" /etc/jupyter/jupyter_notebook_config.py
-sed -i -e "s%__JPYNB_S3_BUCKET_NAME__%${JPYNB_S3_BUCKET_NAME}%g" /etc/jupyter/jupyter_notebook_config.py
+sed -i -e "s%__JPYNB_S3_ACCESS_KEY_ID__%${JPYNB_S3_ACCESS_KEY_ID}%g" /usr/local/etc/jupyter/jupyter_notebook_config.py
+sed -i -e "s%__JPYNB_S3_SECRET_ACCESS_KEY__%${JPYNB_S3_SECRET_ACCESS_KEY}%g" /usr/local/etc/jupyter/jupyter_notebook_config.py
+sed -i -e "s%__JPYNB_S3_REGION_NAME__%${JPYNB_S3_REGION_NAME}%g" /usr/local/etc/jupyter/jupyter_notebook_config.py
+sed -i -e "s%__JPYNB_S3_BUCKET_NAME__%${JPYNB_S3_BUCKET_NAME}%g" /usr/local/etc/jupyter/jupyter_notebook_config.py
+
+cp -arf /usr/local/etc/jupyter /etc/jupyter
 
 docker pull $DOCKER_SPAWNER_IMAGE
 jupyterhub --config=/etc/jupyter/jupyterhub_config.py
