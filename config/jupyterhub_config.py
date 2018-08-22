303eb34c6d72c491f9fa7a3e700a6e510d974466
Bowen Sun <bowensun@gmail.com>
2018-08-22 12:18:31 +0100

    fix notebook config

--- config/jupyterhub_config.py
+++ config/jupyterhub_config.py
@@ -15,4 +15,5 @@ c.Authenticator.admin_users = set([os.environ['ADMIN_USERS']])
 
 c.JupyterHub.spawner_class = 'dockerspawner.DockerSpawner'
 c.DockerSpawner.image = os.environ['DOCKER_SPAWNER_IMAGE']
+c.DockerSpawner.volumes = {'/etc/jupyter/jupyter_notebook_config.py':'/etc/jupyter/jupyter_notebook_config.py'}
 c.Spawner.env_keep = ['PATH']
