272776432105bf98a406a0af865221ec50fdaf49
Bowen Sun <bowensun@gmail.com>
2018-08-23 10:15:06 +0100

    install rsync

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,7 +14,7 @@ sed -i -e "s%__JPYNB_S3_SECRET_ACCESS_KEY__%${JPYNB_S3_SECRET_ACCESS_KEY}%g" /us
 sed -i -e "s%__JPYNB_S3_REGION_NAME__%${JPYNB_S3_REGION_NAME}%g" /usr/local/etc/jupyter/jupyter_notebook_config.py
 sed -i -e "s%__JPYNB_S3_BUCKET_NAME__%${JPYNB_S3_BUCKET_NAME}%g" /usr/local/etc/jupyter/jupyter_notebook_config.py
 
-cp -arf /usr/local/etc/jupyter/* /etc/jupyter/
+rsync -auzv /usr/local/etc/jupyter/ /etc/jupyter/
 
 docker pull $DOCKER_SPAWNER_IMAGE
 jupyterhub --config=/etc/jupyter/jupyterhub_config.py
