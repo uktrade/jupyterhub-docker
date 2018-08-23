96af8e89d894c9019883a144d426dbd3d15292f9
Bowen Sun <bowensun@gmail.com>
2018-08-23 12:57:08 +0100

    bind to local IP

--- config/jupyterhub_config.py
+++ config/jupyterhub_config.py
@@ -3,7 +3,6 @@ import socket
 from oauthenticator.generic import GenericOAuthenticator
 c.JupyterHub.authenticator_class = GenericOAuthenticator
 
-# c.JupyterHub.bind_url = 'https://0.0.0.0:8000'
 c.JupyterHub.hub_ip = '0.0.0.0'
 c.JupyterHub.db_url = os.environ.get('DB_URL', 'sqlite:///jupyterhub.sqlite')
 c.JupyterHub.ssl_cert = '/etc/jupyter/ssl.crt'
