f0d713d3007c5ab23e765a977860006325d676c7
Michal Charemza <michal@charemza.name>
2018-09-10 12:20:16 +0100

    (feat) Better support environment variables with broken newline characters

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-cat <<EOF > /usr/local/etc/jupyter/ssl.key
-$SSL_KEY
-EOF
-
-cat <<EOF > /usr/local/etc/jupyter/ssl.crt
-$SSL_CERT
-EOF
+# The AWS console task definition editor converts \n, a single byte newline character,
+# into \\n, which is the two bytes "\n", so we do the reverse here.
+echo "$SSL_KEY" | sed 's/\\n/\n/g' > /usr/local/etc/jupyter/ssl.key
+echo "$SSL_CERT" | sed 's/\\n/\n/g' > /usr/local/etc/jupyter/ssl.crt
 
 sed -i -e "s%__JPYNB_S3_ACCESS_KEY_ID__%${JPYNB_S3_ACCESS_KEY_ID}%g" /usr/local/etc/jupyter/jupyter_notebook_config.py
 sed -i -e "s%__JPYNB_S3_SECRET_ACCESS_KEY__%${JPYNB_S3_SECRET_ACCESS_KEY}%g" /usr/local/etc/jupyter/jupyter_notebook_config.py
