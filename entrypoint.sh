33b72d0a02c86ac58339ce6bedd225c4c25670e7
Michal Charemza <michal@charemza.name>
2018-09-11 15:05:42 +0100

    (feat) Auto-generate the cert/key for incoming connections to the proxy.
    We are using self-signed beyond the load balancer anyway, so this just reduces
    amount we have to setup.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# The AWS console task definition editor converts \n, a single byte newline character,
-# into \\n, which is the two bytes "\n", so we do the reverse here.
-echo "$SSL_KEY" | sed 's/\\n/\n/g' > /etc/jupyter/ssl.key
-echo "$SSL_CERT" | sed 's/\\n/\n/g' > /etc/jupyter/ssl.crt
+openssl req -new -newkey rsa:2048 -days 3650 -nodes -x509 \
+  -subj /CN=selfsigned \
+  -keyout /etc/jupyter/ssl.key -out /etc/jupyter/ssl.crt
 
 # hub_ip is the interface that the hub listens on, 0.0.0.0 == all
 # hub_connect_ip is the IP that _other_ services will connect to the hub on, i.e. the current private IP address
