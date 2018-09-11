cbdb1f10cb551f660760d25b02c975bc93ffd4be
Michal Charemza <michal@charemza.name>
2018-09-11 14:59:44 +0100

    (refactor) Reduce dependencies and docker build/deploy time

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,6 @@
 ARG JUPYTERHUB_VER=0.9.2
 FROM jupyterhub/jupyterhub:$JUPYTERHUB_VER
 
-RUN apt-get update && \
-    DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends apt-utils && \
-    DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends curl && \
-    apt-get clean && \
-    rm -rf /var/lib/apt/lists/*
-
 RUN pip install --upgrade pip oauthenticator psycopg2-binary
 
 COPY config/jupyterhub_config.py /etc/jupyter/jupyterhub_config.py
--- entrypoint.sh
+++ entrypoint.sh
@@ -8,4 +8,4 @@ echo "$SSL_CERT" | sed 's/\\n/\n/g' > /etc/jupyter/ssl.crt
 
 # hub_ip is the interface that the hub listens on, 0.0.0.0 == all
 # hub_connect_ip is the IP that _other_ services will connect to the hub on, i.e. the current private IP address
-jupyterhub --config=/etc/jupyter/jupyterhub_config.py --JupyterHub.hub_ip="0.0.0.0" --JupyterHub.hub_connect_ip="$(curl -Lfs http://169.254.169.254/latest/meta-data/local-ipv4)"
+jupyterhub --config=/etc/jupyter/jupyterhub_config.py --JupyterHub.hub_ip="0.0.0.0" --JupyterHub.hub_connect_ip="$(wget -qO- http://169.254.169.254/latest/meta-data/local-ipv4)"
