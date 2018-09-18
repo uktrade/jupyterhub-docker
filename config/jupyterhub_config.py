21981a8bbc6fa42c4fc08b088355aa6a00ac76d1
Bowen Sun <bowensun@gmail.com>
2018-09-18 16:09:47 +0100

    disable lab as default

--- config/jupyterhub_config.py
+++ config/jupyterhub_config.py
@@ -9,7 +9,7 @@ c.JupyterHub.ssl_key = '/etc/jupyter/ssl.key'
 
 c.Authenticator.auto_login = True
 c.Authenticator.enable_auth_state = True
-c.Authenticator.admin_users = set([os.environ['ADMIN_USERS']])
+c.Authenticator.admin_users = set(os.environ['ADMIN_USERS'].split())
 
 c.JupyterHub.spawner_class = 'dockerspawner.DockerSpawner'
 c.DockerSpawner.image = os.environ['DOCKER_SPAWNER_IMAGE']
@@ -17,5 +17,5 @@ c.DockerSpawner.volumes = {'/etc/jupyter':'/etc/jupyter'}
 c.DockerSpawner.remove_containers = True
 c.DockerSpawner.debug = True
 c.Spawner.env_keep = ['PATH', 'DATABASE_URL']
-c.Spawner.cmd = ['jupyter-labhub']
-c.Spawner.default_url = '/lab'
+# c.Spawner.cmd = ['jupyter-labhub']
+# c.Spawner.default_url = '/lab'
